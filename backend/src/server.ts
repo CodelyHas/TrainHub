1ba82036ae2ddb1b1b3b53063c8cdf098ffb56f7
CodelyHas <hasan.y.hejazi.b@hotmail.com>
2026-05-14 18:14:15 +0300

    port change

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -29,6 +29,8 @@ app.use("/users", userRoutes);
 app.use("/dashboard", dashboardRoutes);
 app.use("/reports", reportRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
